92bb6489aba0388edb6a9ef9e6019dcf72ded504
Ryan Bateman <ryan.bateman0@gmail.com>
2026-02-17 12:48:23 -0500

    Added center justified text

--- src/components/GuessTable.jsx
+++ src/components/GuessTable.jsx
@@ -6,12 +6,12 @@ export default function GuessTable( {guesses} ) {
         <ol class="relative inset-x-0 top-24 h-16">
             {guesses.map(guess => (
                 <li className="flex justify-center" key={guess[CastawayCatagories[0]]}>
-                    <div className={`w-64 m-1 bg-white text-black border`}>{guess[CastawayCatagories[0]]}</div>
-                    <div className={`w-30 m-1 ${colorLookup[guess[CastawayCatagories[6]][0]]} text-black border`}>{guess[CastawayCatagories[1]]}{arrowLookup[guess[CastawayCatagories[6]][0]]}</div>
-                    <div className={`w-30 m-1 ${colorLookup[guess[CastawayCatagories[6]][1]]} text-black border`}>{guess[CastawayCatagories[2]]}{arrowLookup[guess[CastawayCatagories[6]][1]]}</div>
-                    <div className={`w-30 m-1 ${colorLookup[guess[CastawayCatagories[6]][2]]} text-black border`}>{guess[CastawayCatagories[3]]}</div>
-                    <div className={`w-30 m-1 ${colorLookup[guess[CastawayCatagories[6]][3]]} text-black border`}>{guess[CastawayCatagories[4]]}{arrowLookup[guess[CastawayCatagories[6]][3]]}</div>
-                    <div className={`w-30 m-1 ${colorLookup[guess[CastawayCatagories[6]][4]]} text-black border`}>{guess[CastawayCatagories[5]]}{arrowLookup[guess[CastawayCatagories[6]][4]]}</div>
+                    <div className={`w-64 h-13 m-1 flex items-center justify-center bg-white text-black border`}>{guess[CastawayCatagories[0]]}</div>
+                    <div className={`w-30 m-1 flex items-center justify-center ${colorLookup[guess[CastawayCatagories[6]][0]]} text-black border`}>{guess[CastawayCatagories[1]]}{arrowLookup[guess[CastawayCatagories[6]][0]]}</div>
+                    <div className={`w-30 m-1 flex items-center justify-center ${colorLookup[guess[CastawayCatagories[6]][1]]} text-black border`}>{guess[CastawayCatagories[2]]}{arrowLookup[guess[CastawayCatagories[6]][1]]}</div>
+                    <div className={`w-30 m-1 flex items-center justify-center ${colorLookup[guess[CastawayCatagories[6]][2]]} text-black border`}>{guess[CastawayCatagories[3]]}</div>
+                    <div className={`w-30 m-1 flex items-center justify-center ${colorLookup[guess[CastawayCatagories[6]][3]]} text-black border`}>{guess[CastawayCatagories[4]]}{arrowLookup[guess[CastawayCatagories[6]][3]]}</div>
+                    <div className={`w-30 m-1 flex items-center justify-center ${colorLookup[guess[CastawayCatagories[6]][4]]} text-black border`}>{guess[CastawayCatagories[5]]}{arrowLookup[guess[CastawayCatagories[6]][4]]}</div>
                 </li>
             ))}
         </ol>
